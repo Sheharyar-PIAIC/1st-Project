cd9950c58d1840159ca0299945438ca7eaac57d2
Sheharyar-PIAIC <139269429+Sheharyar-PIAIC@users.noreply.github.com>
2023-07-13 23:10:34 +0500

    Add files via upload
    Hello Everyone. I am Sheharyar and its a great opportunity for us in Batch 47

---
+++ index.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("I am Sheharyar. got an apportuinty to join Batch 47 with more than 1000 students ");
---
+++ index.ts
@@ -0,0 +1,1 @@
+console.log("I am Sheharyar. got an apportuinty to join Batch 47 with more than 1000 students ")
